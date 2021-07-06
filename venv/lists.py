e06c5d3b526a028ea54ae4bee9b7f1257e05373b
DionMS8 <dionsingh8@hotmail.com>
2021-07-06 10:13:48 -0400

    New Commit

--- venv/lists.py
+++ venv/lists.py
@@ -7,3 +7,15 @@ print(saiyans[2])
 print(saiyans[-1]) #LAST ELEMENT HAS AN INDEX POSITION OF -1
 saiyans.pop(0) #REMOVING AN ELEMENT BASED ON A SPECIFIED INDEX POSITION
 saiyans.remove("vegeta") #REMOVING AN ELEMENT BASED ON THE NAME OF THE ELEMENT
+
+
+#LIST1.EXTEND(LIST2)
+#THIS ADDS LIST2 TO LIST1
+
+even = [2,4,6,8]
+odd = [1,3,5,7]
+
+even.extend(odd)
+print(even)
+
+
