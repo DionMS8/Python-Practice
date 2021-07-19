5249e361e5685c19d6e39c3259fc14931d7f137f
DionMS8 <dionsingh8@hotmail.com>
2021-07-18 23:55:06 -0400

    Dates Practice

---
+++ venv/date/classes_objects.py
@@ -0,0 +1,14 @@
+
+
+#USING THE BUILT-IN __INIT__() FUNCTION
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Goku", 69)
+
+print(p1.name)
+print(p1.age)
+
+
