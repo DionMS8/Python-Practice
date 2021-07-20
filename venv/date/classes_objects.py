6d5ff1b8bbb8ef13dc4ddf0894a4c4f3872265d4
DionMS8 <dionsingh8@hotmail.com>
2021-07-19 23:42:16 -0400

    Dates Practice

--- venv/date/classes_objects.py
+++ venv/date/classes_objects.py
@@ -1,14 +1,26 @@
+#******************************* CLASSES/OBJECTS *********************************
 
+#THE BUILT-IN __INIT__() FUNCTION
 
-#USING THE BUILT-IN __INIT__() FUNCTION
-class Person:
-  def __init__(self, name, age):
+#CREATING A CLASS
+class Saiyan:
+  def __init__(self, name, ki, skill, weight):
     self.name = name
-    self.age = age
+    self.ki = ki
+    self.skill = skill
+    self.weight = weight
 
-p1 = Person("Goku", 69)
+#CREATING AN OBJECT
+s1 = Saiyan("Goku", 1000000, "Spirit Bomb", 250) #CREATING AN OBJECT
 
-print(p1.name)
-print(p1.age)
+#PRINTING INFORMATION ABOUT THE OBJECT
+print(s1.name)
+print(s1.ki)
+print(s1.skill)
+print(s1.weight)
+
+
+
+
 
 
